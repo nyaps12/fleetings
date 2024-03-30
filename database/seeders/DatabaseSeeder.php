d923785a9491456d3f90f7a7e2e93605d2220819
Jake Bartolay <68772728+jakebartolay@users.noreply.github.com>
2024-03-31 00:29:13 +0800

    new

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
             SuperAdminSeeder::class,
             RestrictionSeeder::class,
+            VehicleInfoSeeder::class,
         ]);
     }
 }
